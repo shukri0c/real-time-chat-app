6450151bee9cb565fb1aad37cc6eead623a2c865
shukri0c <shukriamahdi05@gmail.com>
2025-09-16 15:46:33 +0100

    created a web socket connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 var app = express();
 var server = app.listen(3000, function(){
@@ -6,3 +7,9 @@ var server = app.listen(3000, function(){
 });
 
 app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+});
